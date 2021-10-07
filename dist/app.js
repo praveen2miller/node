f416d42adcbc21b43b750472d0af0a21a2e00061
praveen2miller <praveenhones@gmail.com>
2021-10-07 10:54:22 +0530

    ts changes

--- dist/app.js
+++ dist/app.js
@@ -5,9 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = (0, express_1.default)();
-var add = function (a, b) { return a + b; };
 app.get('/', function (req, res, next) {
-    console.log(add(5, 5));
     res.send('hello');
 });
 app.listen(5000, function () { return console.log('Server running'); });
