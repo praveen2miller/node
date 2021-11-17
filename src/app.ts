095cb03d786c664517e08ab06f65d162f7c9e9dd
praveen2miller <91452751+praveen2miller@users.noreply.github.com>
2021-11-17 20:11:40 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express,{Application,Request,Response,NextFunction} from 'express';
+let express = require('express');
+let app = express(); // Sensitive
 
-let app:Application=express();
-
-app.get('/',(req:Request,res:Response,next:NextFunction)=>{
-    res.send('hello');
+app.get('/', function (req, res) {
+  res.send('hello')
 });
 
 app.listen(5000,()=>console.log('Server running'));
